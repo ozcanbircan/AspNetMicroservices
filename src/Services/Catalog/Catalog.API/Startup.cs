1f013f8375394fb6ad355c72d014ef75ee99a3ea
Bircan, Ozcan <ozcan.bircan@sap.com>
2021-09-18 16:00:08 -0700

    Added Product

--- src/Services/Catalog/Catalog.API/Startup.cs
+++ src/Services/Catalog/Catalog.API/Startup.cs
@@ -1,20 +1,12 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace Catalog.API
-{
-	public class Startup
-	{
+namespace Catalog.API {
+	public class Startup {
 		public Startup(IConfiguration configuration) {
 			Configuration = configuration;
 		}
@@ -42,9 +34,7 @@ namespace Catalog.API
 
 			app.UseAuthorization();
 
-			app.UseEndpoints(endpoints => {
-				endpoints.MapControllers();
-			});
+			app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
 		}
 	}
 }
