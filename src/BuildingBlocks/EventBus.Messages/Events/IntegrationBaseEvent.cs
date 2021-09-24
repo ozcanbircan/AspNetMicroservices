42a2c12446eafcea3711ba5622187a004fe4ce54
Bircan, Ozcan <ozcan.bircan@sap.com>
2021-09-24 15:38:48 -0700

    Created message events.

---
+++ src/BuildingBlocks/EventBus.Messages/Events/IntegrationBaseEvent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EventBus.Messages.Events {
+
+	public class IntegrationBaseEvent {
+		public IntegrationBaseEvent() {
+			Id = Guid.NewGuid();
+			CreationDate = DateTime.UtcNow;
+		}
+
+		public Guid Id { get; }
+		public DateTime CreationDate { get; }
+	}
+}
