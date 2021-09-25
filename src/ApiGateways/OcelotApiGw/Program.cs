8b19046022696441ce8b3b1440e87263b6183691
Bircan, Ozcan <ozcan.bircan@sap.com>
2021-09-25 14:51:27 -0700

    Added OcelotApiGw project.

---
+++ src/ApiGateways/OcelotApiGw/Program.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace OcelotApiGw {
+	public class Program {
+		public static void Main(string[] args) {
+			CreateHostBuilder(args).Build().Run();
+		}
+
+		public static IHostBuilder CreateHostBuilder(string[] args) =>
+			Host.CreateDefaultBuilder(args)
+			    .ConfigureWebHostDefaults(webBuilder => {
+				     webBuilder.UseStartup<Startup>();
+			     })
+			    .ConfigureLogging((context, builder) => {
+				     builder.AddConfiguration(context.Configuration.GetSection("Logging"));
+				     builder.AddConsole();
+				     builder.AddDebug();
+			     });
+	}
+}
